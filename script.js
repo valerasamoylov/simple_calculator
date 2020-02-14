2d70f49dc8ae71a4c1619f608e0ef081c86a6e35
Valeriy Samoilov <valerasam1012@gmail.com>
2020-02-14 13:07:29 +0200

    add multiply for calculator

--- script.js
+++ script.js
@@ -3,13 +3,15 @@
 $("input[type=checkbox]").on("change", function() {
   let div = $(this).closest(".calculator-inputs");
   $(this).is(":checked")
-    ? div.addClass("choosed-background")
-    : div.removeClass("choosed-background");
+    ? div.addClass("selected-background")
+    : div.removeClass("selected-background");
 });
 
 /* radio checked */
 
 $("#sum").prop("checked", true);
+
+/* if check checkbox will be add*/
 
 $(function() {
   $("#sum").on("change", function() {
@@ -20,9 +22,45 @@ $(function() {
 });
 
 function Add() {
-  let number1 = parseInt($(".row-first").val());
-  let number2 = parseInt($(".row-second").val());
-  let number3 = parseInt($(".row-third").val());
-  var result = number1 + number2 + number3;
+  let number1 = parseInt($("#row-first").val());
+  let number2 = parseInt($("#row-second").val());
+  let number3 = parseInt($("#row-third").val());
+  let number21 = parseInt($("#selected-row-first").val());
+  let number22 = parseInt($("#selected-row-two").val());
+  let number23 = parseInt($("#selected-row-third").val());
+  let number31 = parseInt($("#select-last1").val());
+  let number32 = parseInt($("#select-last2").val());
+  let number33 = parseInt($("#select-last3").val());
+  let result = number1 + number2 + number3;
+  let result2 = number21 + number22 + number23;
+  let result3 = number31 + number32 + number33;
   $("#result-row1").html(result);
+  $("#result-row2").html(result2);
+  $("#result-row3").html(result3);
+}
+
+$(function() {
+  $("#multiply").on("change", function() {
+    if ($("#multiply").prop("checked")) {
+      Multiply();
+    }
+  });
+});
+
+function Multiply() {
+  let number1 = parseInt($("#row-first").val());
+  let number2 = parseInt($("#row-second").val());
+  let number3 = parseInt($("#row-third").val());
+  let number21 = parseInt($("#selected-row-first").val());
+  let number22 = parseInt($("#selected-row-two").val());
+  let number23 = parseInt($("#selected-row-third").val());
+  let number31 = parseInt($("#select-last1").val());
+  let number32 = parseInt($("#select-last2").val());
+  let number33 = parseInt($("#select-last3").val());
+  let result = number1 * number2 * number3;
+  let result2 = number21 * number22 * number23;
+  let result3 = number31 * number32 * number33;
+  $("#result-row1").html(result);
+  $("#result-row2").html(result2);
+  $("#result-row3").html(result3);
 }
